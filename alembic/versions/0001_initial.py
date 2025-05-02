85d46bf35cf921e2d19e371e9a430f63aacc5eba
Diya-0816 <dp2226@njit.edu>
2025-05-03 02:03:59 +0500

    Add files via upload

---
+++ alembic/versions/0001_initial.py
@@ -0,0 +1,30 @@
+"""Initial migration: create users table
+
+Revision ID: 0001_initial
+Revises: 
+Create Date: 2024-05-03 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '0001_initial'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column('email', sa.String(), nullable=False, unique=True),
+        sa.Column('hashed_password', sa.String(), nullable=False),
+        sa.Column('username', sa.String(), nullable=True, unique=True),
+        sa.Column('is_verified', sa.Boolean(), default=False),
+        sa.Column('is_pro', sa.Boolean(), default=False),
+        sa.Column('pro_requested', sa.Boolean(), default=False),
+    )
+
+def downgrade():
+    op.drop_table('users')
